8d8c4cfd8d6c001e3d02940fbde30ac8e66f318d
Paul Knight <paul.knight@state.de.us>
2021-03-02 19:54:54 -0500

    ISSUE-27 Fix import requests in httpbearerauth

--- plugins/module_utils/httpbearerauth.py
+++ plugins/module_utils/httpbearerauth.py
@@ -6,12 +6,12 @@ from __future__ import absolute_import, division, print_function
 __metaclass__ = type
 import traceback
 
+from ansible.module_utils.six import raise_from
+
 # Pull in requests
-HAS_REQUESTS = False
 REQUESTS_IMP_ERR = None
 try:
     import requests
-    HAS_REQUESTS = True
 except ImportError:
     REQUESTS_IMP_ERR = traceback.format_exc()
 
@@ -24,9 +24,10 @@ class HTTPBearerAuth(requests.auth.AuthBase):
 
     def __init__(self, token):
         self.token = token
-        if not HAS_REQUESTS:
-            self.fail(self, msg=missing_required_lib('requests'),
-                      exception=REQUESTS_IMP_ERR)
+        if REQUESTS_IMP_ERR:
+            raise_from(
+                AnsibleError('requests must be installed to use this plugin'),
+                REQUESTS_IMP_ERR)
 
     def __call__(self, r):
         r.headers['Authorization'] = "Bearer {0}".format(str(self.token))
